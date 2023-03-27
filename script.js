bc5e98b15223256f39e04d6b9c8328f49ac856c3
Vineet Vishwakarma <116519930+vineet-vishwakarma@users.noreply.github.com>
2023-03-28 01:38:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ const navLink = document.getElementById('nav-link');
 toggleButton.addEventListener('click', () => {
     navLink.classList.toggle('active')
 })
+
+// Testinomial 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+// FAQs
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
