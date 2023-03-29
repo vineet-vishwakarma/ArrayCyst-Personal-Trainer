fd9e310483bc00ffca2dfe75c2611b4a8adf22eb
Vineet Vishwakarma <116519930+vineet-vishwakarma@users.noreply.github.com>
2023-03-29 21:22:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.toggle("active-faq");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
